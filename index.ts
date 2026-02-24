193fc6bf3670f9043450e7a229243eb66645333e
Chaoscarab <bgvfossen@gmail.com>
2026-02-23 23:13:25 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+// Import the express in typescript file
+import express from 'express';
+import { CopilotClient } from "@github/copilot-sdk";
+import { Copilot } from "./copilot/copilot.js";
+
+
+const app: express.Application = express();
+
+const port: number = 3000;
+const NEON: string = "wss://neonhealth.software/agent-puzzle/challenge"
+const NEON_CODE = "a84e866af58e28e9"
+
+app.get('/', (_req, _res) => {
+    _res.send("beginning challenge...");
+    console.log("[HTTP] / hit - starting copilot chain...");
+    let copilotClient = new CopilotClient();
+    let copilot = new Copilot(copilotClient, NEON_CODE ?? "", NEON);
+    copilot.start().catch((error) => {
+        console.error("[Copilot] start failed:", error);
+    });
+    
+});
+
+
+app.listen(port, () => {
+    console.log(`TypeScript with Express 
+         http://localhost:${port}/`);
+});
